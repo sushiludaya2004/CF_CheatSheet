0b73df6fe7b45af7c0afface257c82090f762d9e
Sushil <127210875+sushiludaya2004@users.noreply.github.com>
2024-03-13 22:51:08 +0530

    Create queueUsing2Stacks.java

---
+++ Queue/queueUsing2Stacks.java
@@ -0,0 +1,27 @@
+//https://www.geeksforgeeks.org/problems/queue-using-two-stacks/1?page=1&category=Queue&sortBy=submissions
+class StackQueue
+{
+    Stack<Integer> s1 = new Stack<Integer>();
+    Stack<Integer> s2 = new Stack<Integer>();
+
+    //Function to push an element in queue by using 2 stacks.
+    void Push(int x)
+    {
+	   s1.push(x);
+    }
+	
+    
+    //Function to pop an element from queue by using 2 stacks.
+    int Pop() {
+    if (s2.isEmpty()) {
+        if (s1.isEmpty()) {
+            return -1;
+        }
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+    }
+    return s2.pop();
+}
+
+}
